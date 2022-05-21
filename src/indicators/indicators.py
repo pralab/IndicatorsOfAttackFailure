127a4b7e98cc7a59e2eee9c3123a1f3b9a2a76cb
maurapintor <maura.pintor@gmail.com>
2022-05-21 17:22:38 +0200

    updated version of the indicators

--- src/indicators/indicators.py
+++ src/indicators/indicators.py
@@ -1,227 +1,274 @@
 import numpy as np
 import pandas as pd
-import sklearn
+import torch
+from secml.adv.attacks import CAttackEvasion, CAttackEvasionFoolbox
+from secml.adv.attacks.evasion.foolbox.secml_autograd import as_tensor, as_carray
 from secml.array import CArray
+from secml.core.constants import inf
+from secml.ml import CClassifier
+from secml.ml.classifiers.reject import CClassifierRejectThreshold, CClassifierDNR
 
-from src.utils.c_metric_score_difference import CMetricScoreDifference
-from src.utils.sampling import sampling_n_sphere
+from src.attacks.smoothed_pgd import CFoolboxAveragedPGD
 
 REJECT_CLASSES = [-1, 10]
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def transfer_failure_indicator(transfer_scores: np.ndarray, pred: int):
-	"""
-	Computes how many attacks (optimized against the surrogate) fail against the real model.
-	:param transfer_scores: the scores computed on the real model
-	:type transfer_scores: np.ndarray
-	:param pred: the label predicted by the surrogate
-	:type pred: int
-	:return: the non-transferability indicator
-	:rtype: bool
-	"""
-	return pred != transfer_scores
+def _extract_input_shape(model: CClassifier):
+    if isinstance(model, CClassifierDNR):
+        return 1, 3, 32, 32
+    elif isinstance(model, CClassifierRejectThreshold):
+        return 1, *model._clf.input_shape
+    else:
+        return 1, *model.input_shape
 
 
-def break_point_angle_indicator(loss: np.ndarray):
-	"""
-	Computes the break-point angle, that quantifies the failure due to small hyperparameters
-	:param loss: the loss computed by the attack, along the attack path
-	:type loss: np.ndarray
-	:return: the break-point indicator
-	:rtype: float
-	"""
-	a, c = np.array([0, loss[0]]), np.array([1, loss[-1]])
-	v1 = c - a
-	v1 /= np.linalg.norm(v1)
-	distances = []
-	for i in range(1, len(loss)):
-		p = np.array([i * 1 / len(loss), loss[i]]) - a
-		distances.append(np.linalg.norm(p - p.T.dot(v1) * v1))
-	best_index = np.argmax(distances) + 1
-	b = np.array([1 / len(loss) * best_index, loss[best_index]])
-	v2 = b - a
-	v2 /= np.linalg.norm(v2)
-	v3 = b - c
-	v3 /= np.linalg.norm(v3)
-	alignment = v2.T.dot(v3)
-	alignment = np.abs(alignment)
-	return alignment
+def interrupted_computational_graph_metric(model: CClassifier) -> bool:
+    """
+    Checks if the model is end-to-end differentiable.
+    :param model: model to test
+    :return: interrupted graph metric
+    """
+    input_shape = (1, *_extract_input_shape(model))
+    random_sample = CArray.rand(input_shape)
+    y = model.decision_function(random_sample)
+    try:
+        model.gradient(random_sample, y)
+    except Exception:
+        return True
+    return False
 
 
-def silent_success_indicator(loss: np.array, scores: np.ndarray, y0: int, y_target: int, adv_y: int):
-	"""
-	Computes the presence of adversarial examples in the path
-	:param loss: the loss computed by the attack, along the attack path
-	:type loss: np.ndarray
-	:param scores: the scores of the attack path, computed against the target
-	:type scores: np.ndarray
-	:param y0: the real label
-	:type y0: int
-	:param y_target: the target label of the attack. None for untarget
-	:type y_target: int
-	:param adv_y: the label after the attack
-	:type adv_y: int
-	:return: the silent success indicator
-	:rtype: bool
-	"""
-	if y_target is None:
-		scores_success = np.argmax(scores, axis=1) != y0
-	else:
-		scores_success = np.argmax(scores, axis=1) == y_target
-	zero_index = np.argmax(scores_success)
-	adv_ratio = (zero_index / loss.shape[0]) if zero_index > 0 else 1
-	attack_success = adv_y != y0 if y_target is None else adv_y == y_target
-	return adv_ratio != 1 and not attack_success
+def zero_gradients_metric(attack: CAttackEvasion, x: CArray) -> bool:
+    """
+    Counts if any of the samples has zero gradients w.r.t. the attack loss.
+    :param attack: attack to be used for the evaluation
+    :param x: batch of samples to use for the test
+    :return: zero gradients metric
+    """
+    preds, scores = attack.classifier.predict(x, return_decision_function=True)
+    n_zero_grads = 0
+    n_samples = preds.shape[0]
+    for i in range(n_samples):
+        # this is a fix required for CAttackEvasionFoolbox
+        attack._y0 = as_tensor(preds[i]) if isinstance(attack, CAttackEvasionFoolbox) else preds[i]
+        grad_norm = attack.objective_function_gradient(x[i, :]).norm(0)
+        n_zero_grads += 1 if grad_norm == 0 else 0
+    return n_zero_grads > 0
 
 
-def increasing_loss_indicator(atk_loss):
-	"""
-	Compute the presence of noisy gradients, by estimating how much the loss is increasing during the optimization
-	:param atk_loss: the loss computed by the attack, along the attack path
-	:type atk_loss: np.ndarray
-	:return: the increasing loss indicator
-	:rtype: float
-	"""
-	increasing = []
-	for i in range(1, atk_loss.shape[0]):
-		diff = atk_loss[i].item() - atk_loss[i - 1].item()
-		v = np.maximum(0.0, diff)
-		if v != 0:
-			increasing.append(atk_loss[i].item())
-			increasing.append(atk_loss[i - 1].item())
-	if len(increasing) < 2:
-		return 0, 0
-	auc = sklearn.metrics.auc(np.linspace(0, 1, len(increasing)), np.array(increasing))
-	st = np.std(increasing)
-	return auc, st
+def unavailable_gradients_indicator(model: CClassifier, attack: CAttackEvasion, x: CArray) -> bool:
+    """
+    Computes if the model has valid and usable gradients.
+    :param model: model to use for the evaluation
+    :param attack: attack to use for the evaluation
+    :param x: batch of input samples
+    :return: the unavailable gradients indicator
+    """
+    interrupted_graph = interrupted_computational_graph_metric(model)
+    zero_grads = zero_gradients_metric(attack, x)
+    return interrupted_graph or zero_grads
 
 
-def bad_init_indicator(y_real, y_pred, y_adv, y_target=None, rejected_class=None):
-	"""
-	Compute if there are successful attacks in the restarts
-	:param y_real: the real label
-	:type y_real: int
-	:param y_pred: the predicted label after the attack
-	:type y_pred: int
-	:param y_adv: the labels obtained through restarts
-	:type y_adv: list
-	:param y_target: the target label of the attack. None for untargeted
-	:type y_target: int
-	:param rejected_class: list of labels that correspond to rejection classes
-	:type rejected_class: list
-	:return: true if at least one restart was successful
-	:rtype: bool
-	"""
-	if rejected_class is None:
-		rejected_class = REJECT_CLASSES
-	if y_target is None:
-		metric = (y_pred == y_real or y_pred in rejected_class) \
-				 and any([y_real != y and y not in rejected_class for y in y_adv])
-	else:
-		metric = y_pred != y_target and any([y_target == y for y in y_adv])
-	return metric
+# TODO this can be implemented with fewer conversions between CArrays, ndarrays and tensors
+def unstable_predictions_indicator(attack: CAttackEvasion, x: CArray, y: CArray, gamma: int = 100,
+                                   radius: float = 8 / 255) -> float:
+    attack._y0 = as_tensor(y) if isinstance(attack, CAttackEvasionFoolbox) else y
+    if isinstance(attack, CFoolboxAveragedPGD):
+        xt = as_tensor(x).repeat_interleave(attack.k, dim=0)
+        attack._y0 = attack._y0.repeat_interleave(attack.k, dim=0)
+        noise = (torch.rand(xt.shape, device=device) - 0.5) * attack.sigma
+        reference_loss = as_tensor(attack.objective_function(as_carray(xt + noise)))
+        reference_loss = as_carray(reference_loss.view(attack.k, x.shape[0]).mean(dim=0))
+    else:
+        reference_loss = attack.objective_function(x)
+    relative_increments = CArray.zeros((gamma, x.shape[0]))
+    divide_reference = CArray(reference_loss.tondarray()).abs()
+    divide_reference[divide_reference == 0] = 1
+    for i in range(gamma):
+        random_directions = (torch.rand(x.shape, device=device) - 0.5) * radius
+        x_perturbed = as_tensor(x) + random_directions
+        if isinstance(attack, CFoolboxAveragedPGD):
+            x_sigma = x_perturbed.repeat_interleave(attack.k, dim=0)
+            noise = (torch.rand(x_sigma.shape, device=device) - 0.5) * attack.sigma
+            avg_loss = as_tensor(attack.objective_function(as_carray(x_sigma + noise)))
+            avg_loss = avg_loss.view(attack.k, x.shape[0])
+            losses = as_carray(avg_loss.mean(dim=0))
+        else:
+            losses = attack.objective_function(as_carray(x_perturbed))
+        relative_increment = CArray.abs(reference_loss - losses)
+        relative_increment /= divide_reference
+        relative_increments[i, :] = relative_increment
+    metric = relative_increments.mean()
+    metric = min(metric, 1.0)
+    return metric
 
 
-def zero_gradients_indicator(grad_norm: np.ndarray, zero_index: int, threshold: float = 0):
-	"""
-	Compute how many of the gradients have zero norm along the path (before finding an adversarial point)
-	:param grad_norm: the norms of the gradient
-	:type grad_norm: np.ndarray
-	:param zero_index: the index of the first adversarial point along the path
-	:type zero_index: int
-	:param threshold: the threshold on the norms of gradients
-	:type threshold: float
-	:return: the zero gradients indicator
-	:rtype: float
-	"""
-	how_many = zero_index if zero_index >= 0 else len(grad_norm)
-	return (grad_norm[:how_many] <= threshold).mean()
+def silent_success_indicator(scores: np.ndarray, y0: int, adv_y: int, y_target: int = None) -> bool:
+    """
+    Computes the presence of adversarial examples in the path.
+    :param scores: the scores of the attack path, computed against the target
+    :param y0: the real label
+    :param y_target: the target label of the attack. None for untargeted
+    :param adv_y: the label after the attack
+    :return: the silent success indicator
+    """
+    if y_target is None:
+        scores_success = np.argmax(scores, axis=1) != y0
+    else:
+        scores_success = np.argmax(scores, axis=1) == y_target
+    attack_success = adv_y != y0 if y_target is None else adv_y == y_target
+    return not attack_success and np.any(scores_success[:-1]).item()
 
 
-def compute_indicators(attack, x, y, clf, transfer_clf,
-					   n_restarts=None, is_patched=False):
-	# run attack original eval
-	y_adv, scores, adv_ds, f_opt = attack.run(x, y)
-
-	y_real = CArray(attack._y0)
-
-	scores_path = clf.decision_function(attack.x_seq).tondarray()
-	transfer_scores = transfer_clf.predict(adv_ds.X)
-
-	y_target = attack.y_target
-
-	grad_path = CArray.zeros(attack.x_seq.shape)
-	for i in range(attack.x_seq.shape[0]):
-		grad_path[i, :] = attack.objective_function_gradient(attack.x_seq[i, :])
-	grad_norms = grad_path.norm_2d(axis=1).tondarray()
-
-	attacker_loss = attack.objective_function(attack.x_seq).tondarray()
-
-	atk_loss_normalized = rescale_loss(attacker_loss)
-	attack_failed = attack_fails(y_adv, y_target, y_real, transfer_scores)
-	_, _, diff = CMetricScoreDifference.score_difference(scores_path,
-														 y_real.tondarray(),
-														 y_target)
-	zero_index = (diff < 0).argmax()
-	if zero_index == 0 and diff[0] >= 0:
-		zero_index = -1
-	increasing_loss, std_loss = increasing_loss_indicator(atk_loss_normalized)
-	break_point_angle = break_point_angle_indicator(atk_loss_normalized)
-	silent_success = silent_success_indicator(attacker_loss, scores_path,
-											  y_real.item(), y_target, y_adv)
-
-	if n_restarts is not None:
-		restart_label_results = []
-		for j in range(n_restarts):
-			perturb_param = attack.epsilon
-			random_restarted_sample = sampling_n_sphere(x.tondarray(),
-														perturb_param, p=np.inf)
-			y_pred_r, scores_r, adv_ds_r, f_obj_r = attack.run(random_restarted_sample, y)
-			if transfer_clf is not None:
-				# if a transfer clf is defined, the restarts
-				# should be evaluated in the transfer clf
-				y_pred_r = transfer_clf.predict(adv_ds_r.X)
-			else:
-				y_pred_r = clf.predict(adv_ds_r.X)
-			restart_label_results.append(y_pred_r)
-
-	bad_init_value = bad_init_indicator(y_real=y_real, y_pred=y_adv,
-										y_adv=restart_label_results,
-										y_target=y_target) if n_restarts is not None else False
-	zero_grads = zero_gradients_indicator(grad_norms, zero_index)
-	transfer_failure = transfer_failure_indicator(transfer_scores, y_adv) \
-		if transfer_scores is not None else False
-	if is_patched:
-		attack_failed = attack_failed \
-						and not bad_init_value \
-						and not silent_success \
-						and not transfer_failure
-	else:
-		attack_failed = attack_failed and not transfer_failure
-
-	df = pd.DataFrame(data={
-		'Attack Success': 0.0 if attack_failed else 1.0,
-		'Silent Success': 1.0 if silent_success else 0.0,
-		'Break-point Angle': break_point_angle,
-		'Increasing Loss': increasing_loss,
-		'Zero Gradients': zero_grads,
-		'Bad Initialization': bad_init_value,
-		'Transfer Failure': transfer_failure,
-	}, index=[0])
-
-	return df
+def always_decreasing(x):
+    """
+    Makes sure a sequence of numbers is always decreasing.
+    :param x: input sequence
+    :return: the transformed monotonically-decreasing sequence
+    """
+    minimum = x[0]
+    for i in range(x.shape[0]):
+        if x[i] < minimum:
+            minimum = x[i]
+        else:
+            x[i] = minimum
+    return x
 
 
 def rescale_loss(atk_loss):
-	if atk_loss.max() != 0:
-		atk_loss -= atk_loss.min()
-		atk_loss /= atk_loss.max()
-	atk_loss = atk_loss.ravel()
-	return atk_loss
+    """
+    Rescales the loss in the [0, 1] interval, handling the case
+    where the loss is always zero.
+    :param atk_loss: loss to be rescaled
+    :return: the rescaled loss
+    """
+    if atk_loss.max() != 0:
+        atk_loss -= atk_loss.min()
+        atk_loss /= atk_loss.max()
+    atk_loss = atk_loss.ravel()
+    return atk_loss
 
 
-def attack_fails(adv_pred, target_label, y0, transfer_scores):
-	untarget_fail = target_label is None and adv_pred == y0
-	targeted_fail = target_label is not None and adv_pred != target_label
-	rejection_failure = adv_pred == -1 or (transfer_scores is not None and transfer_scores in REJECT_CLASSES)
-	return untarget_fail or targeted_fail or rejection_failure
+def incomplete_optimization_indicator(loss):
+    """
+    Checks if the optimization has converged to a minimum
+    :param loss: the loss computed by the attack along the iterations
+    :type loss: np.ndarray
+    :return: the incomplete optimization metric
+    :rtype: float
+    """
+    loss = rescale_loss(loss)
+    loss = always_decreasing(loss)
+    num_steps = min(len(loss), 10)
+    last_steps = loss[-num_steps:]
+    diffs = last_steps.max() - last_steps.min()
+    return float(diffs > 0.05)
+
+
+def transfer_failure_indicator(transfer_scores: np.ndarray, pred: int) -> bool:
+    """
+    Computes how many attacks (optimized against the surrogate) fail against the real model.
+    :param transfer_scores: the scores computed on the real model
+    :param pred: the label predicted by the surrogate
+    :return: the non-transferability indicator
+    """
+    return pred != transfer_scores
+
+
+def unconstrained_attack_failure_indicator(x, y, attack: CAttackEvasion) -> float:
+    """
+    Checks for how many points (among the input points passed)
+    the attack with no bounds reaches the adversarial region.
+    :param x: input samples
+    :param y: label of the input samples
+    :param attack: attack to use for the evaluation
+    :return: the unconstrained attack failure indicator
+    """
+    attack.dmax = inf
+    failed = torch.zeros(x.shape[0])
+
+    # set unconstrained bound
+    if hasattr(attack, 'epsilons'):
+        attack.epsilons = np.inf
+    if hasattr(attack, 'epsilon'):
+        attack.epsilon = np.inf
+
+    for i in range(x.shape[0]):
+        x_i, y_i = x[i, :], y[i]
+        y_pred = attack.classifier.predict(x_i)
+        if y_pred != y_i:
+            failed[i] = 0.0
+            continue
+        adv_label, scores, _, _ = attack.run(x_i, y_i)
+        scores_in_path = attack.classifier.decision_function(attack.x_seq).tondarray()
+        silent = silent_success_indicator(scores_in_path, y0=y_i.item(), adv_y=adv_label, y_target=None)
+        failed[i] = float(not silent and (adv_label == y_pred or adv_label == -1).item())
+    sanity_check_metric = failed.mean().item()
+    return sanity_check_metric
+
+
+def attack_fails(adv_pred, target_label, y0, transfer_scores=None) -> bool:
+    """
+    Checks if the attack fails for the given objective.
+    :param adv_pred: the predicted label of the perturbed sample
+    :param target_label: the target label of the attack (None if untargeted)
+    :param y0: the original label of the unperturbed sample
+    :param transfer_scores: optional predicted label of the perturbed sample
+        when passed to a target model (transfer attack)
+    :return: boolean value, true if the attack succeeded
+    """
+    untarget_fail = target_label is None and adv_pred == y0
+    targeted_fail = target_label is not None and adv_pred != target_label
+    rejection_failure = adv_pred == -1 or (transfer_scores is not None and transfer_scores in REJECT_CLASSES)
+    return untarget_fail or targeted_fail or rejection_failure
+
+
+def compute_indicators(attack, x, y, clf, transfer_clf, is_patched=False) -> pd.DataFrame:
+    """
+    Computes all the indicators of attack failure.
+    :param attack: the attack used for the evaluation
+    :param x: the input samples
+    :param y: the ground-truth labels of the input samples
+    :param clf: classifier to use for crafting the attack
+    :param transfer_clf: classifier to use for testing the attack results
+    :param is_patched: boolean value to be set for patched evaluations (checks
+        for silent success and transfer failures)
+    :return: a dataframe with all the values of the indicators
+    """
+    # run attack
+    y_adv, scores, adv_ds, f_opt = attack.run(x, y)
+
+    # get information required for computing the indicators
+    y_real = CArray(attack._y0)
+    transfer_scores = transfer_clf.predict(adv_ds.X)
+    y_target = attack.y_target
+    attacker_loss = attack.objective_function(attack.x_seq).tondarray()
+    attack_failed = attack_fails(y_adv, y_target, y_real, transfer_scores)
+
+    # compute indicators
+    unavailable_gradients = unavailable_gradients_indicator(clf, attack, x)
+    unstable_predictions = unstable_predictions_indicator(attack, x, y)
+    silent_success = silent_success_indicator(scores, y, y_adv, None)  # can be switched to targeted as well
+    incomplete_optimization = incomplete_optimization_indicator(attacker_loss)
+    transfer_failure = transfer_failure_indicator(transfer_scores, y_adv) \
+        if transfer_scores is not None else False
+    unconstrained_attack_failure = unconstrained_attack_failure_indicator(x, y, attack)
+    if is_patched:
+        attack_failed = attack_failed \
+                        and not silent_success \
+                        and not transfer_failure
+    else:
+        attack_failed = attack_failed and not transfer_failure
+
+    df = pd.DataFrame(data={
+        'attack_success': 0.0 if attack_failed else 1.0,
+        'unavailable_gradients': unavailable_gradients,
+        'unstable_predictions': unstable_predictions,
+        'silent_success': silent_success,
+        'incomplete_optimization': incomplete_optimization,
+        'transfer_failure': transfer_failure,
+        'unconstrained_attack_failure': unconstrained_attack_failure,
+    }, index=[0])
+
+    return df
