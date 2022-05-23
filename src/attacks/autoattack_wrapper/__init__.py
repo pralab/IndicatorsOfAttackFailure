643cf6df14694046d2f1bd5bb38fe149bf63e776
maurapintor <maura.pintor@gmail.com>
2022-05-23 10:12:07 +0200

    fixed apgd wrapper

--- src/attacks/autoattack_wrapper/__init__.py
+++ src/attacks/autoattack_wrapper/__init__.py
@@ -1,4 +1,1 @@
-from .wrapper.autoattack_apgd_attack import CAutoAttackAPGDCE, CAutoAttackAPGDDLR,\
-    CAutoAttackAPGDT
-from .wrapper.autoattack_fab_attack import CAutoAttackFAB, CAutoAttackFABT
-from .wrapper.autoattack_square_attack import CAutoAttackSquare
+from .wrapper.autoattack_apgd_attack import CAutoAttackAPGDCE, CAutoAttackAPGDDLR
