ee741ca6729c6bcdd509fd0b8d1da13b74423419
maurapintor <maura.pintor@gmail.com>
2022-05-23 14:41:54 +0200

    fix script for running experiments

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 for MODEL in kwta distillation pang tws das guo dnr
 do
     echo "Computing indicators for model $MODEL"
-    python3 -m src.ioaf_demo --model $MODEL --samples 2
+    python -m src.ioaf_demo --model $MODEL --samples 2
 done
